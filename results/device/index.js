b63dce293c6cf8d0f3fc4139ac705684073db00c
Sébastien Lerique <sl@mehho.net>
2014-09-10 01:39:38 +0200

    Fool around with d3

--- results/device/index.js
+++ results/device/index.js
@@ -1,5 +1,29 @@
 "use strict";
 $(document).ready(function() {
-  console.log("got here");
+
   $("<p>Which can use Javascript and D3.js goodness.</p>").insertAfter("div#main p:last-child");
+
+  d3.selectAll("p").style("color", function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+  });
+
+  var data = [4, 8, 15, 16, 23, 42];
+  var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 320]);
+  var t = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 2000]);
+
+  d3.select('.chart')
+    .selectAll('div')
+      .data(data)
+    .enter().append('div')
+      .style('width', '0')
+      .text(function(d) { return d; })
+      .transition('linear')
+      .ease('linear')
+      .duration(function(d) { return t(d) })
+      .style('width', function(d) { return x(d) + 'px'; });
+
 });
