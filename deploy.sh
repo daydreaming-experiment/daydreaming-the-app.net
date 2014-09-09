c3233ddca9dec5b50e9fab8b937049f7a510fa64
Sébastien Lerique <sl@mehho.net>
2014-09-09 22:07:40 +0200

    Update update and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "## Ssh-ing to Tal's server to update"
 echo "##"
 echo
 
-ssh daydreaming@74.207.250.156 "cd daydreaming/daydreaming-the-app.net; git pull; ./update.sh"
+ssh daydreaming@74.207.250.156 "cd daydreaming/daydreaming-the-app.net; git pull; workon jekyll; ./update.sh"
 
 echo "##"
 echo "## All done!"
--- update.sh
+++ update.sh
@@ -3,7 +3,13 @@
 
 set -e
 
+# Build
 jekyll build
+
+# Redo symbolic links
 ln -s ../../srv/releases _site/releases
 ln -s ../../srv/experiment-static _site/experiment-static
 ln -s ../../srv/presentations _site/presentations
+
+# Clean up
+rm -f _site/update.sh _site/deploy.sh _site/README.md
