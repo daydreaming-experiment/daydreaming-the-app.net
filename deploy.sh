ffa9ee5d6abdeaefe5f322ca39bd9efe617572b5
Sébastien Lerique <sl@mehho.net>
2014-09-09 22:27:26 +0200

    Get deploy script to work

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ echo "## Ssh-ing to Tal's server to update"
 echo "##"
 echo
 
-ssh daydreaming@74.207.250.156 "cd daydreaming/daydreaming-the-app.net; git pull; workon jekyll; ./update.sh"
+ssh daydreaming@74.207.250.156 'source /etc/bash_completion.d/virtualenvwrapper; workon jekyll; cd daydreaming/daydreaming-the-app.net; git pull; ./update.sh'
 
+echo
 echo "##"
 echo "## All done!"
 echo "##"
