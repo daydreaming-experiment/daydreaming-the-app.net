7acb50dbe99c1f26b1e949dc28ae5c3f9a840409
Sébastien Lerique <sl@mehho.net>
2014-09-09 21:59:09 +0200

    Rename update script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "## Ssh-ing to Tal's server to update"
 echo "##"
 echo
 
-ssh daydreaming@74.207.250.156 "cd daydreaming/daydreaming-the-app.net; git pull; jekyll build; ln -s ../../srv/releases _site/releases; ln -s ../../srv/experiment-static _site/experiment-static; ln -s ../../srv/presentations _site/presentations"
+ssh daydreaming@74.207.250.156 "cd daydreaming/daydreaming-the-app.net; git pull; ./update.sh"
 
 echo "##"
 echo "## All done!"
