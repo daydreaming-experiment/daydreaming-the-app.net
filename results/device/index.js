e124b77137df463dee0618b5901ec6f1c764b4f6
Sébastien Lerique <sl@mehho.net>
2014-09-18 04:15:40 +0200

    Disable d3 stuff

--- results/device/index.js
+++ results/device/index.js
@@ -12,23 +12,23 @@ $(document).ready(function() {
   var nResults = resultsWrap.results.length();
   alert('Number of results: ' + nResults);
 
-  var data = [4, 8, 15, 16, 23, 42];
-  var x = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([0, 320]);
-  var t = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([0, 2000]);
+  //var data = [4, 8, 15, 16, 23, 42];
+  //var x = d3.scale.linear()
+      //.domain([0, d3.max(data)])
+      //.range([0, 320]);
+  //var t = d3.scale.linear()
+      //.domain([0, d3.max(data)])
+      //.range([0, 2000]);
 
-  d3.select('.chart')
-    .selectAll('div')
-      .data(data)
-    .enter().append('div')
-      .style('width', '0')
-      .text(function(d) { return d; })
-      .transition('linear')
-      .ease('linear')
-      .duration(function(d) { return t(d) })
-      .style('width', function(d) { return x(d) + 'px'; });
+  //d3.select('.chart')
+    //.selectAll('div')
+      //.data(data)
+    //.enter().append('div')
+      //.style('width', '0')
+      //.text(function(d) { return d; })
+      //.transition('linear')
+      //.ease('linear')
+      //.duration(function(d) { return t(d) })
+      //.style('width', function(d) { return x(d) + 'px'; });
 
 });
