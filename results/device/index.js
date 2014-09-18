f9e77ec51c7e5a3ef5e1bfa83a1d9adb59059742
Sébastien Lerique <sl@mehho.net>
2014-09-18 04:46:28 +0200

    Further simplify for testing

--- results/device/index.js
+++ results/device/index.js
@@ -1,16 +1,15 @@
 "use strict";
 $(document).ready(function() {
 
-  $("<p>Which can use Javascript and D3.js goodness.</p>").insertAfter("div#main p:last-child");
+  $("<p>App versionCode: " + injectedResults.getVersionCode() + "</p>").insertAfter("div#main p:last-child");
 
   d3.selectAll("p").style("color", function() {
     return "hsl(" + Math.random() * 360 + ",100%,50%)";
   });
 
-  alert('App version code: ' + injectedResults.getVersionCode());
-  var resultsWrap = JSON.parse(injectedResults.getResultsWrap());
-  var nResults = resultsWrap.results.length();
-  alert('Number of results: ' + nResults);
+  //var resultsWrap = JSON.parse(injectedResults.getResultsWrap());
+  //var nResults = resultsWrap.results.length();
+  //alert('Number of results: ' + nResults);
 
   //var data = [4, 8, 15, 16, 23, 42];
   //var x = d3.scale.linear()
